76cb12c0661c664340e6c46a1481376216b71472
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-03-03 22:43:00 +0530

    updated

--- auth/auth.ts
+++ auth/auth.ts
@@ -5,7 +5,7 @@ const auth = async () => {
     if (!token) {
         return null
     }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    const decoded = jwt.verify(token, "SUMUKH@1219");
     return decoded
 }
 
